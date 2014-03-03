23fe912e8b8a8feefcc702ab8987c2a3369644d4
Lucas Marshall <lucas.marshall@uber.com>
2014-03-03 10:03:43 -0800

    Fix sample build and run

---
+++ samples/wordcount/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm /var/lib/storm/supervisor/localstate/1393811855530
+rm /var/lib/storm/supervisor/localstate/1393811855530.version
+rm /var/lib/storm/supervisor/localstate/1393812203595
+rm /var/lib/storm/supervisor/localstate/1393812203595.version
+rm /var/lib/storm/supervisor/localstate/1393812358735
+rm /var/lib/storm/supervisor/localstate/1393812358735.version
+rm /var/lib/storm/supervisor/localstate/1393812574062
+rm /var/lib/storm/supervisor/localstate/1393812574062.version
